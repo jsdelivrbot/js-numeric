8f765b0eb5360b839cb31805f68de404158e6394
Rchockxm <rchockxm.silver@gmail.com>
2015-02-21 20:10:18 +0800

    Create js-numeric-2.js

---
+++ js-numeric-2.js
@@ -0,0 +1,175 @@
+/*
+ * js-numeric - 2.00
+ * Copyright (c) 2013-2015 rchockxm (rchockxm.silver@gmail.com)
+ *
+ * Licensed - under the terms of the MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+ 
+function JsNumeric(targetParams) {
+    var ret = false;
+    var gThis = this;
+    
+    var params = {
+        target: "",
+        callback: null,
+        limit: {
+            max: null,
+            min: null
+        }
+    };
+    
+    if (typeof targetParams === "object") {
+        params.target = (typeof targetParams['target'] === "object") ? targetParams['target'] : params.target;
+        params.callback = (typeof targetParams['callback'] === "function") ? targetParams['callback'] : params.callback;
+        
+        var limit = (typeof targetParams['limit'] === "object") ? targetParams['limit'] : null;
+        
+        if (typeof limit === "object") {
+            params.limit.max = (typeof limit['max'] === "number") ? limit['max'] : params.limit.max;
+            params.limit.min = (typeof limit['min'] === "number") ? limit['min'] : params.limit.min;
+        }
+    }
+    
+    if (typeof params.target === "object") {
+        if (typeof params.target.addEventListener === "function") {
+            params.target.addEventListener("keypress", function(){
+                var result = gThis.setLimit(params.target, params.limit, params.callback);
+            });
+        }
+        else {
+            if (typeof params.target.attachEvent === "object") {
+                params.target.attachEvent("onkeypress", function(){
+                    var result = gThis.setLimit(params.target, params.limit, params.callback);
+                });
+            }
+        }
+        
+        ret = true;
+    }
+    
+    this.setLimit = function (sender, limit, callback) {
+        var ret = false;
+        
+        if (typeof sender !== "object" || typeof limit !== "object") {
+            return ret;
+        }
+        
+        var userSelection = (typeof window.getSelection === "function") ? window.getSelection() : null;
+        
+        if (userSelection === null) {
+            if (document.getSelection != null && document.getSelection != undefined) {
+                userSelection = document.getSelection();
+            }
+            
+            if (userSelection === null) {
+                if (document.selection != null && document.selection != undefined) {
+                    userSelection = window.document.selection.createRange().text;
+                }
+            }
+        }
+        
+        var userSelectPos = sender.value.indexOf(userSelection);
+        var userForcePos = (typeof sender.selectionStart === "number") ? sender.selectionStart : null;
+        
+        if (userForcePos === null) {
+            if (typeof document.selection === "object") {
+                sender.focus();
+                
+                var docSelRange = document.selection.createRange();
+                var senderTextRange = sender.createTextRange();
+                var senderDuplicate = senderTextRange.duplicate();
+                
+                senderTextRange.moveToBookmark(docSelRange.getBookmark());
+                senderDuplicate.setEndPoint("EndToStart", senderTextRange);
+                userForcePos = senderDuplicate.text.length;
+            }
+        }
+        
+        var numMax = null;
+        var numMin = null;
+        
+        if (typeof limit === "object") {
+            numMax = (typeof limit['max'] === "number") ? parseFloat(limit.max) : numMax;
+            numMin = (typeof limit['min'] === "number") ? parseFloat(limit.min) : numMin;
+        }
+        
+        if (typeof limit === "string") {
+            
+        }
+        
+        var dwKeyCode = parseInt(window.event.keyCode);
+        
+        if (dwKeyCode >= 48 && dwKeyCode <= 57) {
+            var newValue = parseInt(dwKeyCode - 48);
+            var oldValue = sender.value;
+            
+            if (userSelection != null) {
+                oldValue = sender.value.replace(userSelection, "");
+            }
+            
+            var globalSuccess = false;
+            var globalMax = false;
+            var globalValue = parseFloat(oldValue + newValue);
+            
+            if (typeof numMax === "number") {
+                if (globalValue <= numMax) {
+                    globalMax = true;
+                    globalSuccess = true;
+                }
+            }
+            
+            if (typeof numMin === "number") {
+                if (globalValue > numMin || (globalValue < 10 && globalValue < numMin)) {
+                    if (globalMax === true) {
+                        globalSuccess = true;
+                    }
+                }
+            }
+            
+            if (globalSuccess === false) {
+                if (typeof callback === "function") {
+                    callback(sender, globalValue, limit);
+                }
+            }
+            
+            ret = globalSuccess;
+        }
+        else {
+            var oldValue = sender.value;
+            
+            if ((userSelection != null && userSelectPos == 0) || (userForcePos == 0)) {
+                if (typeof numMax === "number") {
+                    if (dwKeyCode == 43 && numMax >= 0 && (oldValue.split("+").length - 1 == 0)) {
+                        ret = true;
+                    }
+                }
+                
+                if (typeof numMin === "number") {
+                    if (dwKeyCode == 45 && numMin < 0 && (oldValue.split("-").length - 1 == 0)) {
+                        ret = true;
+                    }
+                }
+            }
+            
+            if ((userSelection != null && userSelectPos > 0) || (userForcePos >= 1)) {
+                if (dwKeyCode == 46 && (oldValue.split(".").length - 1 == 0)) {
+                    ret = true;
+                }
+            }
+        }
+        
+        if (ret === false) {
+            if (typeof event.preventDefault === "function") {
+                event.preventDefault();
+            }
+            else {
+                event.returnValue = false;
+            }
+        }
+        
+        return ret;
+    }
+    
+    return ret;
+}
